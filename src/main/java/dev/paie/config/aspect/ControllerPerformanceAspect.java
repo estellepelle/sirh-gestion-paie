12a0639889fa06fa0f1a5a4685c032141cbd45a5
Estelle PELLE <estellepelle9261@gmail.com>
2017-06-07 13:43:38 +0200

    AOP + persistance BD stable

--- src/main/java/dev/paie/config/aspect/ControllerPerformanceAspect.java
+++ src/main/java/dev/paie/config/aspect/ControllerPerformanceAspect.java
@@ -1,15 +1,23 @@
 package dev.paie.config.aspect;
 
+import java.time.LocalDateTime;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+
+import dev.paie.entite.Performance;
+import dev.paie.repository.PerformanceRepository;
 
 @Configuration
 @Aspect
 public class ControllerPerformanceAspect {
+	
+	@Autowired PerformanceRepository perfRepo;
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(ControllerPerformanceAspect.class);
 
@@ -17,13 +25,23 @@ public class ControllerPerformanceAspect {
 	
 	public Object logPerf(ProceedingJoinPoint pjp) throws Throwable {
 		
-		LOGGER.debug("Début d'exécution de la méthode " + pjp.getSignature().toString());
+		Performance perf = new Performance();
+		LocalDateTime now = LocalDateTime.now();
+		String nomService = pjp.getSignature().toString();
 		
-	    
+		LOGGER.debug("Début d'exécution de la méthode " + nomService);
+		Long before = System.currentTimeMillis();
 		
+	   
 		Object resultat = pjp.proceed();
 		LOGGER.debug("Fin d'exécution de la méthode");
+		Long after = System.currentTimeMillis();
 		
+		perf.setDateHeure(now);
+		perf.setTempsExecution(after - before);
+		perf.setNomService(nomService);
+		
+		perfRepo.saveAndFlush(perf);
 		return resultat;
 		
 	}
